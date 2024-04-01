0643ba3e85954cfa81b87e7c846f2d5eaa095c84
Reza Foroutan <MrHumble10>
2024-04-01 18:49:59 -0300

    modify icon remove delta input

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
-from PIL import Image
 from colorthief import ColorThief
-# import cv2
-import colorsys
-from flask import Flask, render_template, redirect, url_for, abort, flash, request
+from flask import Flask, render_template, request
 import os
-from collections import Counter
 from werkzeug.utils import secure_filename
 
 app = Flask("__name__")
 app.config['SECRET_KEY'] = '123456789'
+# os.environ.get('SECRET_KEY')123456789Rez@Forout@nM@nesh123456789
 app.config["UPLOAD_FILE"] = './static/assets/uploaded_img'
 NAME = 'uploaded_img'
 IMG_FORMAT = ''
 NUM_COLORS = 0
+
 
 @app.route('/', methods=['GET', "POST"])
 def home():
@@ -38,23 +36,9 @@ def home():
 
     if request.method == "POST":
         selected_color = request.form.get('bg-color')
-        #
-        # from prettytable import PrettyTable
-        #
-        # # Assuming 'palette' contains the list of dominant colors
-        # # total_pixels = image_width * image_height  # Get image dimensions
-        # # image_width, image_height = "".join(ct.image.size).split(',')
-        # # print(int(image_width)
-        # # pt = PrettyTable(['Color', 'Count', 'Percentage'])
-        # # for color, count in Counter(palette).items():
-        # #     percentage = int(count / total_pixels * 100)
-        # #     pt.add_row([color, count, percentage])
-        # #
-        # # print(pt)
-        #
-
         return render_template('index.html', colors=colors, selected_color=selected_color,
                                img_address=img_address)
+
     return render_template('index.html', colors=colors,
                            img_address=img_address)
 
@@ -75,51 +59,32 @@ def color_changer():
         NUM_COLORS = NUM_COLORS = int(request.form['numColor'])
         palette = ct.get_palette(color_count=NUM_COLORS, quality=10)
 
-    # plt.imshow([[palette[i-1] for i in range(30)]])
-    # plt.show()
         colors = []
         for color in palette:
-            # print(color)
             colors.append(f"{color[0]:02X}{color[1]:02X}{color[2]:02X}")
-            # print(colorsys.rgb_to_hls(*color))
-            # print(colorsys.rgb_to_hsv(*color))
-
-
-
-
 
         try:
             selected_color = request.form.get('bg-color')
         except KeyError:
             selected_color = colors[0]
         return render_template('new_img.html', colors=colors,
-                                   selected_color=selected_color,
-                                   img_address=img_address, is_selected_color=True)
+                               selected_color=selected_color,
+                               img_address=img_address)
     print(NUM_COLORS, NAME, IMG_FORMAT)
     ct = ColorThief(f'./static/assets/uploaded_img/{NAME}.{IMG_FORMAT}')
     img_address = f'./static/assets/uploaded_img/{NAME}.{IMG_FORMAT}'
 
     palette = ct.get_palette(color_count=NUM_COLORS, quality=10)
 
-    # plt.imshow([[palette[i-1] for i in range(30)]])
-    # plt.show()
     colors = []
     for color in palette:
-        # print(color)
         colors.append(f"{color[0]:02X}{color[1]:02X}{color[2]:02X}")
-        # print(colorsys.rgb_to_hls(*color))
-        # print(colorsys.rgb_to_hsv(*color))
-    selected_color = request.form.get('bg-color')
 
+    selected_color = request.form.get('bg-color')
     return render_template('new_img.html', colors=colors,
                            selected_color=selected_color,
                            img_address=img_address)
 
 
-# @app.route('/b', methods=["GET", "POST"])
-# def n():
-#     if request.method == "POST":
-#         print(request.form.get('bg-color'))
-#     return redirect(url_for('home'))
 if __name__ == "__main__":
     app.run(port=5002, debug=True)
